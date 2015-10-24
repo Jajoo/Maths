660093b0821ddf78597ae8c5f964cf6c4b7e902f
Jajoo <hersheyman123@hotmail.com>
2015-10-23 19:59:08 -0600

    Maths3.0
    this is a program to do my maths for me

---
+++ maths3.py
@@ -0,0 +1,18 @@
+M = float(input("Please enter the M: ")) #This is for the constnat (usually the slope)
+B = float(input('Please enter the B: '))#This is for the other constant (usally where the slope starts on the line
+newX = float(input("Please enter the new X: ")) #This is for the new X
+
+def Mx_B():
+    M
+    B
+    print (M,"x +", B)
+    newX
+    while True:
+        hello = int(input("Please enter the new X: ")) #This is for the new X
+        print (m * hello + b)
+        print (M * newX + B)
+
+
+
+Mx_B()
+
